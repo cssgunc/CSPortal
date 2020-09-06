c96a5e71bdc285be2759ed784a15abe641cfb205
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-09-06 14:17:00 -0400

    Updated Airtable link

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -23,6 +23,12 @@ function Landing() {
       });
   }, [airtableKey]);
 
+  // PLEASE READ:
+  // to access the data for all the announcements, use the 'announcements' variable initialized above.
+  // check out the structure of the data in your browser's console (in the developer tools).
+  // check out the code below for an example of how to access and render the different variables.
+  // use this link to see how the AirTable is set up: https://airtable.com/invite/l?inviteId=invUlo8HaLCbPq2ZC&inviteToken=1be33a9d46def5811f63b756c370cbdec080b3d799521b6bc1ac99abb63d37a6
+
   return (
     <div>
       <section className="section is-white">
--- src/components/Opportunities/index.js
+++ src/components/Opportunities/index.js
@@ -26,7 +26,7 @@ function Opportunities() {
   // to access the data for all the opportunities, use the 'opportunities' variable initialized above.
   // check out the structure of the data in your browser's console (in the developer tools).
   // check out the /Landing/index.js for an example of how to access and render the different variables.
-  // use this link to see how the API works: https://airtable.com/appWPIPmVSmXaMhey/api/docs#curl/table:opportunities
+  // use this link to see how the AirTable is set up: https://airtable.com/invite/l?inviteId=invUlo8HaLCbPq2ZC&inviteToken=1be33a9d46def5811f63b756c370cbdec080b3d799521b6bc1ac99abb63d37a6
 
   return (
     <div>
