225b1d162b08075d463f06b0f1ac90b5e4d2a9ba
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-09-06 14:24:26 -0400

    Updated Airtable API link access instructions

--- src/components/Webinars/index.js
+++ src/components/Webinars/index.js
@@ -26,7 +26,11 @@ function Webinars() {
   // to access the data for all the webinars, use the 'webinars' variable initialized above.
   // check out the structure of the data in your browser's console (in the developer tools).
   // check out the /Landing/index.js for an example of how to access and render the different variables.
-  // use this link to see how the AirTable is set up: https://airtable.com/invite/l?inviteId=invUlo8HaLCbPq2ZC&inviteToken=1be33a9d46def5811f63b756c370cbdec080b3d799521b6bc1ac99abb63d37a6
+  // use this link to see how the AirTable is set up:
+  // https://airtable.com/invite/l?inviteId=invUlo8HaLCbPq2ZC&inviteToken=1be33a9d46def5811f63b756c370cbdec080b3d799521b6bc1ac99abb63d37a6
+  // here's how you can access the different fields in the database
+  // (you need click the above link first and log into/create an account with Airtable before you can access this link):
+  // https://airtable.com/appWPIPmVSmXaMhey/api/docs#curl/table:videos
 
   return (
     <div>
