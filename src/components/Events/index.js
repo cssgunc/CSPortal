04766d00fa898976ca65a34ab0810b7727875299
Pich Him <pichhim@live.unc.edu>
2020-09-10 10:15:55 -0400

    Added embedded calendar into Events

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { withAuthorization } from '../Session';
 import Heading from '../General/Heading';
+import ViewWithTopBorder from '../General/ViewWithTopBorder';
 
 function Events() {
   return (
     <div>
+      <ViewWithTopBorder>
       <section className="section is-white">
-        <Heading>Events</Heading>
+        <Heading>Calendar</Heading>
       </section>
+      
+      <div>
+      <iframe
+              title="RTCDirectoryAirtableView"
+              className="airtable-embed"
+              src="https://calendar.google.com/calendar/embed?src=rewritingthecode.org_kfhaeluivti168r0cbn5hj40qs%40group.calendar.google.com&ctz=America%2FNew_York"
+              frameBorder="0"
+              width="100%"
+              height="533"
+            />
+      </div>
+      </ViewWithTopBorder>
     </div>
   );
 }
