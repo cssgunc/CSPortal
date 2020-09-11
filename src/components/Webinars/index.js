27992e3034d9e211929b0a7d57c09e969a84b7cc
daqichen <DChen19st@bishopstang.org>
2020-09-11 15:22:00 -0400

    Same as Sample PR

--- src/components/Webinars/index.js
+++ src/components/Webinars/index.js
@@ -32,10 +32,37 @@ function Webinars() {
   // (you need click the above link first and log into/create an account with Airtable before you can access this link):
   // https://airtable.com/appWPIPmVSmXaMhey/api/docs#curl/table:videos
 
+  if (webinars.length > 5) {
+    setWebinars(webinars.slice(0, 5));
+  }
+
   return (
     <div>
       <section className="section is-white">
         <Heading>Webinars</Heading>
+        {webinars.length === 0 ? (
+          <div className="box">
+            <div className="content">
+              <p>
+                <strong>No videos yet! Check back later :)</strong>
+              </p>
+            </div>
+          </div>
+        ) : (
+          webinars.map((vid) => (
+            <div className="box" key={vid.id}>
+              <div className="content">
+                <p>
+                  <a href={vid.fields.VideoLink}>
+                    <strong>{vid.fields.Title}</strong>
+                  </a>
+                  <br />
+                  {vid.fields.Description}
+                </p>
+              </div>
+            </div>
+          ))
+        )}
       </section>
     </div>
   );
