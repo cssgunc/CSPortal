6fa5521cb145cc019aa5b139ae08614b2df216ca
Pich Him <pichhim@live.unc.edu>
2020-09-18 16:00:11 -0400

    React BigCalendar added

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withAuthorization } from '../Session';
 import Heading from '../General/Heading';
 import ViewWithTopBorder from '../General/ViewWithTopBorder';
+import axios from 'axios';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 function Events() {
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const calendarId = "rewritingthecode.org_kfhaeluivti168r0cbn5hj40qs@group.calendar.google.com";
+  const [events, setEvents] = useState([]);
+  const localizer = momentLocalizer(moment);
+
+  useEffect(() => {
+    axios 
+      .get(
+        `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`,
+      )
+      .then((result) => {
+        setEvents(result.data.items);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [apiKey, calendarId]);
+
   return (
     <div>
       <ViewWithTopBorder>
@@ -11,14 +34,13 @@ function Events() {
         <Heading>Calendar</Heading>
       </section>
       <div>
-      <iframe
-              title="RTCDirectoryAirtableView"
-              className="airtable-embed"
-              src="https://calendar.google.com/calendar/embed?src=rewritingthecode.org_kfhaeluivti168r0cbn5hj40qs%40group.calendar.google.com&ctz=America%2FNew_York"
-              frameBorder="0"
-              width="100%"
-              height="533"
-            />
+        <Calendar
+          localizer = {localizer}
+          // TODO: FORMAT events for BigCalendar
+          // TODO: Use another Calendar framework that includes description + time too
+          events={events}
+          style={{height: 500}}
+        />
       </div>
       </ViewWithTopBorder>
     </div>
