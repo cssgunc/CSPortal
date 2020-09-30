aa0fab9a1277c4ea33a1dfa92de3e58dcb027831
Pich Him <pichhim@live.unc.edu>
2020-09-29 22:13:23 -0400

    Formatted company partners data from airtable and tested basic design

--- src/components/CompanyPartners/index.js
+++ src/components/CompanyPartners/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { withAuthorization } from '../Session';
-import Heading from '../General/Heading';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { withAuthorization } from "../Session";
+import Heading from "../General/Heading";
 
 function CompanyPartners() {
   const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY;
@@ -32,10 +32,50 @@ function CompanyPartners() {
   // (you need click the above link first and log into/create an account with Airtable before you can access this link):
   // https://airtable.com/appWPIPmVSmXaMhey/api/docs#curl/table:companies
 
+  const formattedCompanies = companies.map((company) => {
+    return {
+      // Logo -> Name -> Description -> Opportunities
+      logo: company.fields.Logo[0].thumbnails.full.url,
+      name: company.fields.Name,
+      description: company.fields.Description,
+    };
+  });
+  console.log(formattedCompanies);
+
   return (
     <div>
       <section className="section is-white">
         <Heading>Company Partners</Heading>
+      </section>
+      <section className="section">
+        {/* <body>
+            {formattedCompanies.map(obj => (
+              <li>
+                {obj.name} - {obj.description}
+              </li>
+            ))}
+          </body> */}
+          
+        {formattedCompanies.map((obj) => (
+          <div className="box">
+            <article className="media">
+              <div className="media-left is-vcentered">
+                <figure className="image is-128x128">
+                <img src={obj.logo} alt={obj.name + " Logo"}></img>
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                    <div className="title">
+                      <strong>{obj.name}</strong>
+                    </div>
+                    <br></br>
+                    {obj.description}
+                </div>
+              </div>
+            </article>
+          </div>
+        ))}
       </section>
     </div>
   );
