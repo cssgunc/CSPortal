1a274cfdba3343801001a643c61bae60d421d6dc
chardorn <charleydorn@gmail.com>
2020-10-05 21:01:09 -0400

    simple-update
    ready to pull request

--- src/components/MeetTheTeam/index.js
+++ src/components/MeetTheTeam/index.js
@@ -1,133 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import { withAuthorization } from '../Session';
 import Heading from '../General/Heading';
 import ViewWithTopBorder from '../General/ViewWithTopBorder';
 import colors from '../../constants/RTCColors';
+import axios from 'axios';
+
+
+//import axios from 'axios';
 
 
 function MeetTheTeam() {
+
+  const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY;
+
+  //Example Data
+  //Real data to be imported from AirTable
+  const data = [
+      {id: "1", fields: {
+        name: "George Harrison",
+        bio: "this is me, george bleh blah hello"
+          }},
+      {id: "2", fields: {
+        name: "John Lennon",
+        bio: "wndklwend lennon weridjerod"
+      }},
+      {id: "3", fields: {
+        name: "Paul McCartney",
+        bio: "dkjfnrkfwekldfj"
+      }},
+      {id: "4", fields: {
+        name: "Ringo",
+        bio: "dkjfnrkfwekldfj"
+      }}
+    ];
+    
+
   return (
     <div>
     <section className="section is-white">
     <Heading>Meet The Team</Heading>
     </section>
     <section className="section">
-        <div class="columns">
-          <div class="column">
-            <div className="container" style={{textAlign: 'center', alignContent: 'center'}}>
-              <ViewWithTopBorder color={colors.darkBlue}>
-              <Heading style={{marginTop: '20px'}}>
-                  Sabrina Somal
-              </Heading>
-              <figure class= "image" style={{margin: '10%'}}>
-                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
-              </figure>
-              <p>
-              Lorem ipsum dolor sit amet, est assum discere reprimique ut, magna iisque qui ut. No pro diam populo, ea veri paulo senserit mei. 
-              Vim ea nostro menandri corrumpit, putant conclusionemque qui at. Wisi ludus ponderum est te. Per exerci omittam deseruisse id, wisi altera laoreet cu mel.
-              His te intellegam interesset. Ex ocurreret gubergren scriptorem cum, ex per consul fabellas voluptatibus. Ut nam expetenda iracundia. 
-              Meliore oportere elaboraret et sea, eam ut tincidunt definiebas intellegam, duo no esse detracto noluisse. Ex duo aperiam discere gubergren, libris mucius possim te nec. 
-              </p>
-              </ViewWithTopBorder>
-              
+        <div className="columns">
+             {data.slice(0, 10).map((person) => (
+               <div className="column">
+                <div className="container" key={person.id} style={{textAlign: 'center', alignContent: 'center'}}>   
+                  <ViewWithTopBorder color={colors.darkBlue}>
+                    <Heading style={{marginTop: '20px'}}>
+                      {person.fields.name}
+                    </Heading>
+                    <figure className= "image" style={{margin: '10%'}}>
+                        <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
+                    </figure>
+                    <br />
+                    <p className="card-content">
+                    {person.fields.bio}
+                  </p>
+                  </ViewWithTopBorder>
+                  </div>
+                  </div>
+              ))}
             </div>
-          </div>
-          <div class="column">
-            <div className="container" style={{textAlign: 'center'}}>
-            <ViewWithTopBorder color={colors.darkBlue}>
-              <Heading style={{marginTop: '20px'}}>
-                  Jalen Johnson
-              </Heading>
-              <figure class= "image" style={{margin: '10%'}}>
-                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
-              </figure>
-              <p>
-              Lorem ipsum dolor sit amet, est assum discere reprimique ut, magna iisque qui ut. No pro diam populo, ea veri paulo senserit mei. 
-              Vim ea nostro menandri corrumpit, putant conclusionemque qui at. Wisi ludus ponderum est te. Per exerci omittam deseruisse id, wisi altera laoreet cu mel.
-              His te intellegam interesset. Ex ocurreret gubergren scriptorem cum, ex per consul fabellas voluptatibus. Ut nam expetenda iracundia. 
-              Meliore oportere elaboraret et sea, eam ut tincidunt definiebas intellegam, duo no esse detracto noluisse. Ex duo aperiam discere gubergren, libris mucius possim te nec. 
-              </p>
-              </ViewWithTopBorder>
-            </div>
-          </div>
-          <div class="column">
-            <div className="container" style={{textAlign: 'center'}}>
-            <ViewWithTopBorder color={colors.darkBlue}>
-              <Heading style={{marginTop: '20px'}}>
-                Robby Rurtzewitz
-              </Heading>
-              <figure class= "image" style={{margin: '10%'}}>
-                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
-              </figure>
-              <p>
-              Lorem ipsum dolor sit amet, est assum discere reprimique ut, magna iisque qui ut. No pro diam populo, ea veri paulo senserit mei. 
-              Vim ea nostro menandri corrumpit, putant conclusionemque qui at. Wisi ludus ponderum est te. Per exerci omittam deseruisse id, wisi altera laoreet cu mel.
-              His te intellegam interesset. Ex ocurreret gubergren scriptorem cum, ex per consul fabellas voluptatibus. Ut nam expetenda iracundia. 
-              Meliore oportere elaboraret et sea, eam ut tincidunt definiebas intellegam, duo no esse detracto noluisse. Ex duo aperiam discere gubergren, libris mucius possim te nec. 
-              </p>
-              </ViewWithTopBorder>
-            </div>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <div className="container" style={{textAlign: 'center', alignContent: 'center'}}>
-              <ViewWithTopBorder color={colors.darkBlue}>
-              <Heading style={{marginTop: '20px'}}>
-                  Sabrina Somal
-              </Heading>
-              <figure class= "image" style={{margin: '10%'}}>
-                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
-              </figure>
-              <p>
-              Lorem ipsum dolor sit amet, est assum discere reprimique ut, magna iisque qui ut. No pro diam populo, ea veri paulo senserit mei. 
-              Vim ea nostro menandri corrumpit, putant conclusionemque qui at. Wisi ludus ponderum est te. Per exerci omittam deseruisse id, wisi altera laoreet cu mel.
-              His te intellegam interesset. Ex ocurreret gubergren scriptorem cum, ex per consul fabellas voluptatibus. Ut nam expetenda iracundia. 
-              Meliore oportere elaboraret et sea, eam ut tincidunt definiebas intellegam, duo no esse detracto noluisse. Ex duo aperiam discere gubergren, libris mucius possim te nec. 
-              </p>
-              </ViewWithTopBorder>
-              
-            </div>
-          </div>
-          <div class="column">
-            <div className="container" style={{textAlign: 'center'}}>
-            <ViewWithTopBorder color={colors.darkBlue}>
-              <Heading style={{marginTop: '20px'}}>
-                  Jalen Johnson
-              </Heading>
-              <figure class= "image" style={{margin: '10%'}}>
-                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
-              </figure>
-              <p>
-              Lorem ipsum dolor sit amet, est assum discere reprimique ut, magna iisque qui ut. No pro diam populo, ea veri paulo senserit mei. 
-              Vim ea nostro menandri corrumpit, putant conclusionemque qui at. Wisi ludus ponderum est te. Per exerci omittam deseruisse id, wisi altera laoreet cu mel.
-              His te intellegam interesset. Ex ocurreret gubergren scriptorem cum, ex per consul fabellas voluptatibus. Ut nam expetenda iracundia. 
-              Meliore oportere elaboraret et sea, eam ut tincidunt definiebas intellegam, duo no esse detracto noluisse. Ex duo aperiam discere gubergren, libris mucius possim te nec. 
-              </p>
-              </ViewWithTopBorder>
-            </div>
-          </div>
-          <div class="column">
-            <div className="container" style={{textAlign: 'center'}}>
-            <ViewWithTopBorder color={colors.darkBlue}>
-              <Heading style={{marginTop: '20px'}}>
-                Robby Rurtzewitz
-              </Heading>
-              <figure class= "image" style={{margin: '10%'}}>
-                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
-              </figure>
-              <p>
-              Lorem ipsum dolor sit amet, est assum discere reprimique ut, magna iisque qui ut. No pro diam populo, ea veri paulo senserit mei. 
-              Vim ea nostro menandri corrumpit, putant conclusionemque qui at. Wisi ludus ponderum est te. Per exerci omittam deseruisse id, wisi altera laoreet cu mel.
-              His te intellegam interesset. Ex ocurreret gubergren scriptorem cum, ex per consul fabellas voluptatibus. Ut nam expetenda iracundia. 
-              Meliore oportere elaboraret et sea, eam ut tincidunt definiebas intellegam, duo no esse detracto noluisse. Ex duo aperiam discere gubergren, libris mucius possim te nec. 
-              </p>
-              </ViewWithTopBorder>
-            </div>
-          </div>
-        </div>
-        </section>
+      </section>
     </div>
   );
 }
