74453a3a0ab0de734cdabd6cc4957d4d76a871dc
Pich Him <pichhim@live.unc.edu>
2020-10-06 21:18:53 -0400

    Use Airtable to retrieve Calendar ID instead of hard-coding

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { withAuthorization } from '../Session';
 import Heading from '../General/Heading';
 import ViewWithTopBorder from '../General/ViewWithTopBorder';
@@ -7,7 +8,23 @@ import GoogleCalendar from '../General/GoogleCalendar';
 
 
 function Events() {
-  const calendarId = "rewritingthecode.org_kfhaeluivti168r0cbn5hj40qs@group.calendar.google.com"; // RTC general calendar
+  const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY;
+  // stores Google Calendar ID 
+  const [calendarId, setCalendarId] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.airtable.com/v0/appWPIPmVSmXaMhey/MasterLinks`, {
+        headers: { Authorization: `Bearer ${airtableKey}` },
+      })
+      .then((result) => {
+        setCalendarId(result.data.records.filter(obj => obj.fields.Name === "EventsCalendar")[0].fields.Link);
+        console.log(result.data.records.filter(obj => obj.fields.Name === "EventsCalendar")[0].fields.Link);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [airtableKey]);
 
   return (
     <div className="column">
