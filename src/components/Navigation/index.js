3f72ec389aeee01e2534f1598f83c9b9d763e212
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-10-10 17:49:02 -0400

    made navbar mobile friendly and added logo

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import SignOutButton from '../SignOut';
 import { AuthUserContext } from '../Session';
 import colors from '../../constants/RTCColors';
+import logo from '../../constants/RTC_Color_Logo.png';
 
 function Navigation() {
   const authUser = useContext(AuthUserContext);
@@ -13,24 +14,53 @@ function Navigation() {
 const styles = {
   bar: {
     backgroundColor: colors.black,
+    height: '75px',
+    paddingLeft: '20px',
+  },
+  barMobile: {
+    height: '100%',
+  },
+  burger: {
+    height: '100%',
+    color: colors.white,
+    marginRight: '12px',
+    backgroundColor: colors.black,
+    border: 'none',
+  },
+  burgerLine: {
+    width: '20px',
+  },
+  dropdown: {
+    backgroundColor: colors.black,
   },
   menu: {
-    padding: '0',
+    padding: '0px 0px 0px 13px',
+    backgroundColor: colors.black,
   },
   start: {
-    display: 'flex',
     alignItems: 'center',
-  },
-  first: {
-    marginLeft: '10px',
   },
   lightButton: {
     backgroundColor: colors.lightBlue,
     color: colors.white,
   },
+  signInBar: {
+    width: '100%',
+  },
+  signInContainer: {
+    marginLeft: 'auto',
+    height: '100%',
+  },
+  signInButton: {
+    marginBottom: '0',
+    backgroundColor: colors.green,
+    color: colors.white,
+  },
 };
 
 const NavigationAuthYes = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div>
       <nav
@@ -39,20 +69,36 @@ const NavigationAuthYes = () => {
         aria-label="main navigation"
         style={styles.bar}
       >
-        <div id="navbar" className="navbar-menu is-active" style={styles.menu}>
+        <div className="navbar-brand" style={styles.barMobile}>
+          <Link className="navbar-item" to={ROUTES.LANDING}>
+            <img src={logo} alt="Rewriting the Code Portal Home" />
+          </Link>
+          <button
+            className={`navbar-burger ${openMenu ? 'is-active' : ''}`}
+            type="button"
+            style={styles.burger}
+            onClick={() => setOpenMenu(!openMenu)}
+            tabIndex={0}
+          >
+            <span aria-hidden="true" style={styles.burgerLine} />
+            <span aria-hidden="true" style={styles.burgerLine} />
+            <span aria-hidden="true" style={styles.burgerLine} />
+          </button>
+        </div>
+        <div
+          id="navbarMenu"
+          className={`navbar-menu ${openMenu ? 'is-active' : ''}`}
+          style={styles.menu}
+        >
           <div className="navbar-start" style={styles.start}>
-            <Link
-              className="navbar-item"
-              to={ROUTES.LANDING}
-              style={styles.first}
-            >
+            <Link className="navbar-item" to={ROUTES.LANDING}>
               Home
             </Link>
             <div className="navbar-item has-dropdown is-hoverable">
               <Link className="navbar-item" to={ROUTES.LANDING}>
                 About Us
               </Link>
-              <div className="navbar-dropdown" style={styles.bar}>
+              <div className="navbar-dropdown" style={styles.dropdown}>
                 <Link className="navbar-item" to={ROUTES.MEETTHETEAM}>
                   Meet the Team
                 </Link>
@@ -68,7 +114,7 @@ const NavigationAuthYes = () => {
               <Link className="navbar-item" to={ROUTES.RESOURCES}>
                 Resources
               </Link>
-              <div className="navbar-dropdown" style={styles.bar}>
+              <div className="navbar-dropdown" style={styles.dropdown}>
                 <Link className="navbar-item" to={ROUTES.OPPORTUNITIES}>
                   Opportunities
                 </Link>
@@ -96,13 +142,13 @@ const NavigationAuthYes = () => {
             <Link className="navbar-item" to={ROUTES.ACCOUNT}>
               Account
             </Link>
-            <Link className="navbar-item" to={ROUTES.ADMIN}>
+            {/* <Link className="navbar-item" to={ROUTES.ADMIN}>
               Admin
-            </Link>
+            </Link> */}
           </div>
-          <div className="navbar-end">
-            <Link className="navbar-item" to={ROUTES.REFER}>
-              <div className="buttons">
+          <div className="navbar-end is-active">
+            <div className="buttons">
+              <Link className="navbar-item" to={ROUTES.REFER}>
                 <button
                   className="button is-light"
                   style={styles.lightButton}
@@ -110,10 +156,10 @@ const NavigationAuthYes = () => {
                 >
                   Refer
                 </button>
+              </Link>
+              <div className="navbar-item">
+                <SignOutButton />
               </div>
-            </Link>
-            <div className="navbar-item">
-              <SignOutButton />
             </div>
           </div>
         </div>
@@ -132,26 +178,21 @@ const NavigationAuthNo = () => {
         style={styles.bar}
       >
         <div
-          id="navbarBasicExample"
-          className="navbar-menu is-active"
-          style={styles.menu}
+          className="navbar-brand"
+          style={{ ...styles.barMobile, ...styles.signInBar }}
         >
-          <div className="navbar-start" style={styles.start}>
-            <Link
-              className="navbar-item"
-              to={ROUTES.LANDING}
-              style={styles.first}
-            >
-              Home
-            </Link>
-          </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <Link className="button is-light" to={ROUTES.SIGN_IN}>
-                  Sign In
-                </Link>
-              </div>
+          <Link className="navbar-item" to={ROUTES.LANDING}>
+            <img src={logo} alt="Rewriting the Code Portal Home" />
+          </Link>
+          <div className="navbar-end" style={styles.signInContainer}>
+            <div className="navbar-item" style={styles.signInContainer}>
+              <Link
+                className="button is-light"
+                to={ROUTES.SIGN_IN}
+                style={styles.signInButton}
+              >
+                Sign In
+              </Link>
             </div>
           </div>
         </div>
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <div className="buttons">
-    <button
-      className="button is-light"
-      onClick={firebase.doSignOut}
-      type="button"
-    >
-      Sign Out
-    </button>
-  </div>
+  <button
+    className="button is-light"
+    onClick={firebase.doSignOut}
+    type="button"
+  >
+    Sign Out
+  </button>
 );
 
 export default withFirebase(SignOutButton);
