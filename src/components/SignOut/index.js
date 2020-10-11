ff8a10edc734e70ad1624ba2eb094904ef3de180
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-10-11 00:48:55 -0400

    added profile icon on navbar

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { withFirebase } from '../Firebase';
 
+const styles = {
+  button: {
+    marginBottom: 0,
+  },
+};
+
 const SignOutButton = ({ firebase }) => (
   <button
     className="button is-light"
     onClick={firebase.doSignOut}
     type="button"
+    style={styles.button}
   >
     Sign Out
   </button>
