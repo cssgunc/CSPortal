e0ad98c589996451fca76768f7940d50b87c75c2
Rebecca Rozansky <beckybriercreek@gmail.com>
2020-10-13 20:13:25 -0400

    adding backend to contact us page

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -2,12 +2,47 @@ import React from 'react';
 import { withAuthorization } from '../Session';
 import Heading from '../General/Heading';
 
+const nodemailer = require('nodemailer');
+
+
+function Mail_send(){
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+
+    auth: {
+        user: 'testnodemailercssg@gmail.com', // generated ethereal user
+        pass: 'Test123!'  // generated ethereal password
+    },
+
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact" <testnodemailercssg@gmail.com>', // sender address
+      to: 'calciumphosphate0@gmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+
+  };
+
+  // send mail with defined transport object
+   transporter.sendMail(mailOptions).then(function(response){
+     console.log("email sent");
+   })
+   .catch(function(error){
+     console.log("Error: ",error);
+   });
+}
+
 function ContactUs() {
+
+
   return (
     <div>
       <section className="section is-white">
         <Heading>Contact Us</Heading>
-      </section>
+
+    </section>
     </div>
   );
 }
