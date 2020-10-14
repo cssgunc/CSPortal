4df840aa397e0c79b31cc1957f7cf4e5dfb85ed0
Eden Chou <eden.chou0606@gmail.com>
2020-10-13 20:26:19 -0400

    adding opportunity data

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,12 +1,4 @@
 /* eslint-disable react/no-unescaped-entities */
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "bulma/css/bulma.css";
-import Heading from "../General/Heading";
-import ViewWithTopBorder from "../General/ViewWithTopBorder";
-import colors from "../../constants/RTCColors";
-=======
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bulma/css/bulma.css';
@@ -16,20 +8,14 @@ import Heading from '../General/Heading';
 import ViewWithTopBorder from '../General/ViewWithTopBorder';
 import colors from '../../constants/RTCColors';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
 import 'font-awesome/css/font-awesome.min.css';
 import GoogleCalendar from '../General/GoogleCalendar';
 import ScrollMenu from 'react-horizontal-scrolling-menu';
 
 function Landing() {
   const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY;
-<<<<<<< HEAD
-  const calendarId = "rewritingthecode.org_kfhaeluivti168r0cbn5hj40qs@group.calendar.google.com";
-=======
-  const calendarKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const calendarId =
     'rewritingthecode.org_kfhaeluivti168r0cbn5hj40qs@group.calendar.google.com';
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
 
   // all the announcements data is stored here
   const [announcements, setAnnouncements] = useState([]);
@@ -72,7 +58,6 @@ function Landing() {
     },
   };
 
-<<<<<<< HEAD
   const [opportunities, setOpportunities] = useState([]);
 
   useEffect(() => {
@@ -80,14 +65,6 @@ function Landing() {
       .get(`https://api.airtable.com/v0/appWPIPmVSmXaMhey/Opportunities`, {
         headers: { Authorization: `Bearer ${airtableKey}` },
       })
-=======
-  // To implement calendar
-  useEffect(() => {
-    axios
-      .get(
-        `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${calendarKey}`,
-      )
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
       .then((result) => {
         setOpportunities(result.data.records);
         console.log(result.data.records);
@@ -95,36 +72,7 @@ function Landing() {
       .catch((error) => {
         console.log(error);
       });
-<<<<<<< HEAD
   }, [airtableKey]);
-=======
-  }, [calendarKey, calendarId]);
-
-  // Formats events to suit React BigCalendar event objects
-  const formattedEvents = events.map((obj) => {
-    return {
-      title: obj.summary,
-      start: new Date(obj.start.dateTime.slice(0, 19)),
-      end: new Date(obj.end.dateTime.slice(0, 19)),
-      allDay: false,
-      resource: null,
-    };
-  });
-
-  // Edit styling for events in Calendar
-  const eventStyleCreator = () => {
-    const style = {
-      backgroundColor: '#CADDD1', // Light green from Figma calendar
-      borderRadius: '0px',
-      opacity: 0.8,
-      color: 'black',
-      display: 'block',
-    };
-    return {
-      style,
-    };
-  };
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
 
   // PLEASE READ:
   // to access the data for all the announcements, use the 'announcements' variable initialized above.
@@ -143,16 +91,10 @@ function Landing() {
           <div className="column is-6">
             <ViewWithTopBorder color={colors.green}>
               <Heading>Calendar</Heading>
-<<<<<<< HEAD
-              <GoogleCalendar eventsColor = {colors.lightGreen} calendarId = {calendarId}></GoogleCalendar>
-=======
-              <Calendar
-                localizer={localizer}
-                events={formattedEvents}
-                style={{ height: 650 }}
-                eventPropGetter={eventStyleCreator}
-              />
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
+              <GoogleCalendar
+          eventsColor={colors.lightGreen}
+          calendarId={calendarId}
+        />
             </ViewWithTopBorder>
           </div>
           <div className="column is-6">
@@ -179,55 +121,6 @@ function Landing() {
             </div>
             <div className="columns is-variable is-2">
               <div className="column">
-<<<<<<< HEAD
-                <ViewWithTopBorder style={styles.oppsSize} color={colors.limeGreen}>
-                <div style={styles.oppsHeader}><div><Heading>Opportunities</Heading></div>
-                <div style={styles.oppsLineHeight}>
-                <a style={styles.oppsNav}>See All</a>
-                <a><span className="icon"><i className="fa fa-angle-left" aria-hidden="true"></i></span></a>
-                <a><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span></a></div>
-                </div>
-                <div className="columns is-multiline">
-            {opportunities.length === 0 ? (
-              <div className="box">
-                <div className="content">
-                  <p>
-                    <strong>No Opportunities yet! Check back later! </strong>
-                  </p>
-                </div>
-              </div>
-            ) : (
-              opportunities.filter((role)=> role.fields.Featured).map((role) => (
-                <div className="column is-one-quarter">
-                  <div className="box" key={role.id}>
-                    <img src={role.fields.CompanyLogo[0].thumbnails.small.url}alt="Logo"></img>
-                    <div className="content">
-                      <p>
-                        <a id={role.id} href={'/opportunities/' + role.id}>
-                          <strong>{role.fields.Title}</strong>
-                        </a>
-                        <br />
-                        {role.fields.CompanyName}
-                        <br />
-                        {role.fields.Location}
-                        <br />
-                        {role.fields.Start}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))
-              
-            )}
-          </div>
-                {/* <div className="tile is-ancestor">
-                  <div className="tile is-parent">
-                    <div class="tile is-child box">
-                      <p class="title is-4">Opportunity</p>
-                      <p class="subtitle is-6">image & info here</p>
-                      <figure class="image is-4by3">
-                      </figure>
-=======
                 <ViewWithTopBorder
                   style={styles.oppsSize}
                   color={colors.limeGreen}
@@ -235,7 +128,6 @@ function Landing() {
                   <div style={styles.oppsHeader}>
                     <div>
                       <Heading>Opportunities</Heading>
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
                     </div>
                     <div style={styles.oppsLineHeight}>
                       <a href="/" style={styles.oppsNav}>
@@ -253,33 +145,66 @@ function Landing() {
                       </a>
                     </div>
                   </div>
-                  <div className="tile is-ancestor">
-                    <div className="tile is-parent">
-                      <div className="tile is-child box">
-                        <p className="title is-4">Opportunity</p>
-                        <p className="subtitle is-6">image & info here</p>
-                        <figure className="image is-4by3" />
-                      </div>
-                    </div>
-                    <div className="tile is-parent">
-                      <div className="tile is-child box">
-                        <p className="title is-4">Opportunity</p>
-                        <p className="subtitle is-6">image & info here</p>
-                        <figure className="image is-4by3" />
-                      </div>
-                    </div>
-                    <div className="tile is-parent">
-                      <div className="tile is-child box">
-                        <p className="title is-4">Opportunity</p>
-                        <p className="subtitle is-6">image & info here</p>
-                        <figure className="image is-4by3" />
+                  <div className="columns is-multiline">
+            {opportunities.length === 0 ? (
+              <div className="box">
+                <div className="content">
+                  <p>
+                    <strong>No Opportunities yet! Check back later! </strong>
+                  </p>
+                </div>
+              </div>
+            ) : (
+              opportunities
+                .filter((role) => role.fields.Featured)
+                .map((role) => (
+                  <div className="column is-one-quarter">
+                    <div className="box" key={role.id}>
+                      <img
+                        src={role.fields.CompanyLogo[0].thumbnails.small.url}
+                        alt="Logo"
+                      />
+                      <div className="content">
+                        <p>
+                          <a id={role.id} href={`/opportunities/${role.id}`}>
+                            <strong>{role.fields.Title}</strong>
+                          </a>
+                          <br />
+                          {role.fields.CompanyName}
+                          <br />
+                          {role.fields.Location}
+                          <br />
+                          {role.fields.Start}
+                        </p>
                       </div>
                     </div>
                   </div>
-<<<<<<< HEAD
-                </div> */}
-=======
->>>>>>> 765ec34fedd51d6edf5674242ebcb4334dfb3893
+                ))
+            )}
+          </div>
+                  {/* <div className="tile is-ancestor">
+                    <div className="tile is-parent">
+                      <div className="tile is-child box">
+                        <p className="title is-4">Opportunity</p>
+                        <p className="subtitle is-6">image & info here</p>
+                        <figure className="image is-4by3" />
+                      </div>
+                    </div>
+                    <div className="tile is-parent">
+                      <div className="tile is-child box">
+                        <p className="title is-4">Opportunity</p>
+                        <p className="subtitle is-6">image & info here</p>
+                        <figure className="image is-4by3" />
+                      </div>
+                    </div>
+                    <div className="tile is-parent">
+                      <div className="tile is-child box">
+                        <p className="title is-4">Opportunity</p>
+                        <p className="subtitle is-6">image & info here</p>
+                        <figure className="image is-4by3" />
+                      </div>
+                    </div>
+                  </div> */}
                 </ViewWithTopBorder>
               </div>
             </div>
