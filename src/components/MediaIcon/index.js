353169cdbab1eba8f4d7acba5da33dfb8194ddf4
Braden Goodwin <6876787+BradenGoodwin@users.noreply.github.com>
2021-03-17 22:52:13 -0400

    Add styles for media icon

--- src/components/MediaIcon/index.js
+++ src/components/MediaIcon/index.js
@@ -2,7 +2,31 @@ import React from "react";
 import colors from "../../constants/RTCColors";
 
 function MediaIcon(props) {
-  return <div>icon</div>;
+  const styles = {
+    icon: {
+      filter: "grayscale(100%) brightness(0.0)",
+      cursor: "pointer",
+      margin: "1px",
+      padding: "0px",
+    },
+    large: {
+      height: "25px",
+      width: "25px",
+    },
+    small: {
+      height: "10px",
+      width: "10px",
+    },
+  };
+
+  const src = props.src;
+  const alt = props.alt;
+
+  return (
+    <div style={styles.icon}>
+      <img src={src} alt={alt} />
+    </div>
+  );
 }
 
 export default MediaIcon;
