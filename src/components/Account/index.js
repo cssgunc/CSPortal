f864acb917eb4b9bf07d89fb20480f087183c41d
Komal Essarani <komalessarani@gmail.com>
2021-03-22 19:12:35 -0400

    can retrieve record id

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -3,8 +3,23 @@ import { withAuthorization } from '../Session';
 
 import * as ROUTES from '../../constants/routes';
 
+var Airtable = require('airtable');
+const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY;
+var base = new Airtable({apiKey: airtableKey}).base('appWPIPmVSmXaMhey');
+
 function Account(props) {
   const { authUser } = props;
+
+  // get record id 
+  base('Directory').select({
+    filterByFormula: `{Email} = "${authUser.email}"`
+  }).eachPage(function page(records, fetchNextPage) {
+    records.forEach(function(record) {
+        console.log('Retrieved', record.id);
+	    console.dir(record);  // show full record JS object
+    })
+  });
+  
   return (
     <div>
       <section className="section is-white">
