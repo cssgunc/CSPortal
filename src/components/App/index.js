20a6b024946527e03c39f48703ae7a5cc84f5134
Eden Chou <eden.chou0606@gmail.com>
2021-04-19 18:28:35 -0400

    added dependencies

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -27,6 +27,7 @@ import OpportunitiesSubPage from "../OpportunitiesSubPage";
 import WebinarsSubPage from "../WebinarsSubPage";
 import VerifyPage from "../Verify";
 import Footer from "../Footer";
+import ProfilePage from "../ProfilePage";
 import Announcements from "../Announcements";
 
 function App(props) {
