41f305b4d6100ccf7beef3edd4acdb8017243437
Braden Goodwin <6876787+BradenGoodwin@users.noreply.github.com>
2021-04-21 17:34:34 -0400

    Display all clubs as links for testing

--- src/components/Communities/index.js
+++ src/components/Communities/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { withAuthorization } from '../Session';
-import Heading from '../General/Heading';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { withAuthorization } from "../Session";
+import Heading from "../General/Heading";
+import * as ROUTES from "../../constants/routes";
 
 function Communities() {
+  const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY;
+  // all the clubs data is stored here
+  const [clubs, setClubs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.airtable.com/v0/appWPIPmVSmXaMhey/Clubs`, {
+        headers: { Authorization: `Bearer ${airtableKey}` },
+      })
+      .then((result) => {
+        setClubs(result.data.records);
+        console.log(result.data.records);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [airtableKey]);
+
   return (
     <div>
       <section className="section is-white">
         <Heading>Communities</Heading>
+        {clubs.map((club) => (
+          <p>
+            <Link to={`${ROUTES.COMMUNITIES}/${club.id}`}>
+              {club.fields.Name}
+            </Link>
+            <br />
+          </p>
+        ))}
       </section>
     </div>
   );
